4cc9421869d28e55e1aa684f87a700008c253886
Rohit Jha <rjha1@godaddy.com>
2025-02-14 09:14:01 +0530

    add varibles, constants and types

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Variable declaration
+	var name string = "Alice"
+	var age int = 18
+	var height float64 = 5.1
+	var isDeveloper bool = false
+
+	// Type inference
+	city := "Wonderland"
+
+	// Constants
+	const monarch = "Red Queen"
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Height:", height)
+	fmt.Println("Is Developer?", isDeveloper)
+	fmt.Println("City:", city)
+	fmt.Println("Monarch:", monarch)
 }
