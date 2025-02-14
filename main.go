d68e096ad313da5441462f542629a1b8e1cb82ae
Rohit Jha <rjha1@godaddy.com>
2025-02-14 09:19:07 +0530

    control statements

--- main.go
+++ main.go
@@ -3,22 +3,77 @@ package main
 import "fmt"
 
 func main() {
-	// Variable declaration
-	var name string = "Alice"
-	var age int = 18
-	var height float64 = 5.1
-	var isDeveloper bool = false
+	// If-Else Statement
+	num := 10
+	if num%2 == 0 {
+		fmt.Println(num, "is even")
+	} else {
+		fmt.Println(num, "is odd")
+	}
 
-	// Type inference
-	city := "Wonderland"
+	fmt.Println()
 
-	// Constants
-	const monarch = "Red Queen"
+	// For Loop
+	fmt.Println("Counting from 1 to 5:")
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+	}
 
-	fmt.Println("Name:", name)
-	fmt.Println("Age:", age)
-	fmt.Println("Height:", height)
-	fmt.Println("Is Developer?", isDeveloper)
-	fmt.Println("City:", city)
-	fmt.Println("Monarch:", monarch)
+	fmt.Println()
+
+	// While Loop
+	fmt.Println("Counting from 5 to 1:")
+	i := 5
+	for i >= 1 {
+		fmt.Println(i)
+		i--
+	}
+
+	fmt.Println()
+
+	// Nested Loop
+	fmt.Println("Multiplication Table:")
+	for i := 1; i <= 5; i++ {
+		for j := 1; j <= 10; j++ {
+			fmt.Print(i*j, "\t")
+		}
+		fmt.Println()
+	}
+
+	fmt.Println()
+
+	// Break Statement
+	fmt.Println("Counting from 1 to 10:")
+	for i := 1; i <= 10; i++ {
+		if i == 6 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	// Continue Statement
+	fmt.Println("Counting from 1 to 10 (skipping 6):")
+	for i := 1; i <= 10; i++ {
+		if i == 6 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	// Switch Case
+	day := "Friday"
+	switch day {
+	case "Monday":
+		fmt.Println("Start of the week!")
+	case "Friday":
+		fmt.Println("Weekend eve!")
+	case "Saturday", "Sunday":
+		fmt.Println("Weekend!")
+	default:
+		fmt.Println("Hang in there!")
+	}
 }
