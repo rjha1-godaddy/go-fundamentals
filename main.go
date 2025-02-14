e185d39b5a0be62a4c4ad0fd60f94070d8fc0982
Rohit Jha <rjha1@godaddy.com>
2025-02-14 12:19:07 +0530

    json response

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Response struct {
+	Message string `json:"message"`
+	Status  string `json:"status"`
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	response := Response{
+		Message: "Hello, World!",
+		Status:  "success",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
 	http.HandleFunc("/hello", helloHandler)
-	fmt.Println("Server is running on http://localhost:8080")
+
+	fmt.Println("Server is running at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
