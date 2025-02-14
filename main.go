b5fe97be283fe3c8fa2cb240563c917b204e80fc
Rohit Jha <rjha1@godaddy.com>
2025-02-14 12:14:01 +0530

    simple http server

--- main.go
+++ main.go
@@ -1,48 +1,16 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"net/http"
 )
 
-var (
-	ErrInsufficientFunds = errors.New("insufficient funds")
-	ErrNegativeAmount    = errors.New("amount cannot be negative")
-)
-
-type Account struct {
-	Balance float64
-}
-
-func (a *Account) Withdraw(amount float64) error {
-	if amount < 0 {
-		return fmt.Errorf("withdrawal failed: %w", ErrNegativeAmount)
-	}
-	if amount > a.Balance {
-		return fmt.Errorf("withdrawal failed: %w", ErrInsufficientFunds)
-	}
-
-	a.Balance -= amount
-	return nil
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
 }
 
 func main() {
-	acc := &Account{Balance: 100}
-
-	transactions := []float64{50, 200, -10}
-
-	for _, amount := range transactions {
-		err := acc.Withdraw(amount)
-		if err != nil {
-			if errors.Is(err, ErrInsufficientFunds) {
-				fmt.Println("Error: You don't have enough balance.")
-			} else if errors.Is(err, ErrNegativeAmount) {
-				fmt.Println("Error: Invalid amount entered.")
-			} else {
-				fmt.Println("Error:", err)
-			}
-		} else {
-			fmt.Println("Withdrawal successful! New Balance:", acc.Balance)
-		}
-	}
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
